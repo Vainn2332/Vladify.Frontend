86a354d13f437df2cdf1c4da79bf617a8d2aeda5
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-07 16:01:26 +0200

    docompose main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HomePage } from "./pages/HomePage/HomePage.tsx";
+import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );

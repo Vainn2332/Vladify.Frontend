19417bb50885f21b5248b255bc2f80785ca8de40
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-10 12:05:44 +0200

    add default route

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,4 +6,12 @@ export const router = createBrowserRouter([
     path: "/",
     element: <HomePage />,
   },
+  {
+    path: "*",
+    element: (
+      <div style={{ padding: 32, textAlign: "center" }}>
+        <h2>404 — Страница не найдена</h2>
+      </div>
+    ),
+  },
 ]);

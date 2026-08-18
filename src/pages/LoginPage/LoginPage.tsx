35ebd0f534c86b131cbe00779e9364c46a62bb1b
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-18 14:21:35 +0200

    update login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,9 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import logo from "../../assets/logo.png";
 
 export function LoginPage() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <div className="bg-app flex h-dvh flex-col p-4">
       <div className="bg-login-page-card shadow-card mx-auto mt-[25dvh] flex w-full max-w-sm flex-col rounded-2xl border-2 border-gray-300 p-7">
@@ -13,7 +16,10 @@ export function LoginPage() {
           <h1 className="text-2xl font-bold">Vladify</h1>
         </div>
         <p>Please log in to continue.</p>
-        <button className="mt-4 cursor-pointer rounded-xl bg-blue-500 px-4 py-2 text-white transition-all hover:-translate-y-1 hover:bg-blue-600">
+        <button
+          onClick={() => loginWithRedirect()}
+          className="mt-4 cursor-pointer rounded-xl bg-blue-500 px-4 py-2 text-white transition-all hover:-translate-y-1 hover:bg-blue-600"
+        >
           Log In
         </button>
       </div>

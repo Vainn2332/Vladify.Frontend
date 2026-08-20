ccd16c46cc06a5c57a10cf2ac74ec64ab14b16d1
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-20 12:53:21 +0200

    update login page responsiveness

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,15 +1,20 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
 
 export function LoginPage() {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isLoading } = useAuth0();
+  const [isLoggingIn, setIsLoggingIn] = useState(false);
   const handleLogin = () => {
+    setIsLoggingIn(true);
     loginWithRedirect();
   };
 
+  const isDisabled = isLoading || isLoggingIn;
+
   return (
-    <div className="bg-app flex h-dvh flex-col p-4">
-      <div className="bg-login-page-card shadow-card mx-auto mt-[25dvh] flex w-full max-w-sm flex-col rounded-2xl border-2 border-gray-300 p-7">
+    <div className="bg-app flex min-h-dvh flex-col items-center justify-center p-4 pb-32">
+      <div className="bg-login-page-card shadow-card flex w-full max-w-sm flex-col rounded-2xl border-2 border-gray-300 p-7">
         <div className="relative flex items-center justify-center gap-2">
           <img
             src={logo}
@@ -21,9 +26,10 @@ export function LoginPage() {
         <p>Please log in to continue.</p>
         <button
           onClick={handleLogin}
-          className="mt-4 cursor-pointer rounded-xl bg-blue-500 px-4 py-2 text-white transition-all hover:-translate-y-1 hover:bg-blue-600"
+          disabled={isDisabled}
+          className="mt-4 cursor-pointer rounded-xl bg-blue-500 px-4 py-2 text-white transition-all hover:-translate-y-1 hover:bg-blue-600 disabled:cursor-not-allowed disabled:opacity-70"
         >
-          Log In
+          {isLoggingIn ? "Redirecting..." : "Log In"}
         </button>
       </div>
     </div>

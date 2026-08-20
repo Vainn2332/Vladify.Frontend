7cb5b624963106d37515baf0f6e8c0db3a2d09a8
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-20 13:22:14 +0200

    add protected route component

---
+++ src/components/Auth/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate, Outlet } from "react-router-dom";
+
+export function ProtectedRoute() {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="flex min-h-dvh justify-center">
+        <p> Loading... </p>
+      </div>
+    );
+  }
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+}

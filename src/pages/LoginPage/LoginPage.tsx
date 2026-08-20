cb8a7ad098557e97bc8508493005765d6ccbb89a
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-20 13:52:28 +0300

    Feature/add auth page (#3)
    * add tailwindCSS
    
    * add prettier formatter for tailwind
    
    * delete stylelint
    
    * update index css
    
    * match components with updated index.css
    
    * fixed css
    
    * add logo image
    
    * create login page
    
    * update routing
    
    * update index.css
    
    * install auth0 package
    
    * add aut provider in main
    
    * update login page
    
    * move  button clock handler to variable

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,31 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import logo from "../../assets/logo.png";
+
+export function LoginPage() {
+  const { loginWithRedirect } = useAuth0();
+  const handleLogin = () => {
+    loginWithRedirect();
+  };
+
+  return (
+    <div className="bg-app flex h-dvh flex-col p-4">
+      <div className="bg-login-page-card shadow-card mx-auto mt-[25dvh] flex w-full max-w-sm flex-col rounded-2xl border-2 border-gray-300 p-7">
+        <div className="relative flex items-center justify-center gap-2">
+          <img
+            src={logo}
+            alt="Vladify Logo"
+            className="absolute left-0 aspect-square h-12 mix-blend-multiply"
+          />
+          <h1 className="text-2xl font-bold">Vladify</h1>
+        </div>
+        <p>Please log in to continue.</p>
+        <button
+          onClick={handleLogin}
+          className="mt-4 cursor-pointer rounded-xl bg-blue-500 px-4 py-2 text-white transition-all hover:-translate-y-1 hover:bg-blue-600"
+        >
+          Log In
+        </button>
+      </div>
+    </div>
+  );
+}

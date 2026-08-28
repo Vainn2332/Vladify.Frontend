378f6f9a23c363f73b6324a39948310953bd1624
Vladislav Pomozov <vladuk2332@gmail.com>
2026-08-28 14:28:06 +0200

    add my playlists page

---
+++ src/pages/MyPlaylistsPage/MyPlaylistsPage.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import {
+  CardSection,
+  type CardSectionItem,
+} from "../../components/CardSection/CardSection";
+import { CirclePlus } from "lucide-react";
+import { CreatePlaylistModal } from "../../components/Modals/CreatePlaylistModal";
+
+const MY_PLAYLISTS: CardSectionItem[] = [
+  {
+    id: "playlist-1",
+    title: "someTitle",
+    imageUrl: "https://picsum.photos/seed/track10/300/300",
+  },
+];
+
+export function MyPlaylistsPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCreatePlaylist = (title: string) => {
+    console.log("Создаем плейлист:", title);
+  };
+
+  return (
+    <div className="bg-app flex flex-1 flex-col">
+      <CardSection
+        items={MY_PLAYLISTS}
+        title="My playlists"
+        action={
+          <button
+            type="button"
+            onClick={() => setIsModalOpen(true)}
+            className="cursor-pointer hover:opacity-90"
+          >
+            <CirclePlus className="size-10 fill-[#e2e8f0] stroke-black" />
+          </button>
+        }
+      />
+
+      <CreatePlaylistModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSubmit={handleCreatePlaylist}
+      />
+    </div>
+  );
+}

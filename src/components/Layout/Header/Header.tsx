4261feedad5d6629c934cd145706de1e25299e0d
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-03 18:07:43 +0200

    add searchInput component

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
+import { SearchInput } from "../../SearchInput/SearchInput";
+import "./Header.css";
 
 export function Header() {
   return (
-    <div className="grid grid-cols-3 items-center gap-2 bg-linear-to-r from-cyan-300/70 to-teal-500/40 p-4 text-sm md:text-base lg:text-2xl">
+    <header className="grid grid-cols-3 items-center gap-2 bg-linear-to-r from-cyan-300/70 to-teal-500/40 p-4 text-sm md:text-base lg:text-2xl">
       <div className="flex items-center justify-center gap-2">
         <Link to={"/"}>
           <img
@@ -15,14 +17,10 @@ export function Header() {
         </Link>
         <Link to={"/MyPlaylists"}>My playlists</Link>
       </div>
-      <input
-        type="text"
-        placeholder="Search"
-        className="w-full max-w-80 justify-self-center rounded-2xl bg-white/85 py-1 text-center text-cyan-950 transition-colors duration-200 outline-none hover:bg-white/70 focus:bg-white/70"
-      ></input>
+      <SearchInput className="w-full max-w-80 justify-self-center" />
       <div className="flex justify-center">
         <Link to="/Logout">Quit</Link>
       </div>
-    </div>
+    </header>
   );
 }
---
+++ src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,15 @@
+import type { ComponentPropsWithRef } from "react";
+
+export function SearchInput({
+  className,
+  ...props
+}: ComponentPropsWithRef<"input">) {
+  return (
+    <input
+      type="text"
+      placeholder="Search"
+      {...props}
+      className={`rounded-2xl bg-white/85 py-1 text-center text-cyan-950 transition-colors duration-200 outline-none hover:bg-white/70 focus:bg-white/70 ${className}`}
+    />
+  );
+}

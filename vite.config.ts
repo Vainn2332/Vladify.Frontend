19f8c791d896f0aacc5fde91c693eeb828dd761f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-04 17:29:44 +0300

    Refactor/add explicit web server port on startup (#10)
    * update port
    
    * update env example

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: { port: 5173, strictPort: true },
+  preview: { port: 5173, strictPort: true },
 });

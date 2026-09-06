8798fb7b8f495ef00605fbbe0b2297bdd4ee9278
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-06 22:32:31 +0300

    add paginationParams

--- src/api/services/usePlaylistsService.ts
+++ src/api/services/usePlaylistsService.ts
@@ -1,9 +1,10 @@
 import { useMemo } from "react";
 import { useApiClient } from "../useApiClient";
 import type { CreatePlaylistDto, playlist } from "../dtos/playlist";
+import type { PaginationParams } from "../dtos/paginationParams";
 
 export interface PlaylistsService {
-  getAll(): Promise<playlist[]>;
+  getAll(params: PaginationParams): Promise<playlist[]>;
   getById(id: string): Promise<playlist>;
   add(dto: CreatePlaylistDto): Promise<playlist>;
   delete(id: string): Promise<void>;
@@ -14,7 +15,8 @@ export function usePlaylistsService(): PlaylistsService {
 
   return useMemo(
     () => ({
-      getAll: () => api.get<playlist[]>("/playlists").then((r) => r.data),
+      getAll: (params) =>
+        api.get<playlist[]>("/playlists", { params }).then((r) => r.data),
 
       getById: (id) =>
         api.get<playlist>(`/playlists/${id}`).then((r) => r.data),

d14aa11a990b5762506efbb3a22c2fba3a9b84af
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-20 17:46:19 +0300

    add eol

--- src/api/dtos/playlist.ts
+++ src/api/dtos/playlist.ts
@@ -11,17 +11,17 @@ export interface createPlaylistDto {
   name: string;
 }
 
-export interface addSongToPlaylistDto{
-  playlistId:string;
-  songId:string;
+export interface addSongToPlaylistDto {
+  playlistId: string;
+  songId: string;
 }
 
-export interface updatePlaylistDto{
-  id:string
-  name:string;
+export interface updatePlaylistDto {
+  id: string;
+  name: string;
 }
 
-export interface deleteSongFromPlaylistDto{
-  playlistId:string;
-  songId:string;
+export interface deleteSongFromPlaylistDto {
+  playlistId: string;
+  songId: string;
 }

64ea30bfcfa85f3b461dc8c5b49e5272acf3c71f
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-20 17:42:34 +0300

    update api

--- src/api/services/usePlaylistsService.ts
+++ src/api/services/usePlaylistsService.ts
@@ -1,43 +1,65 @@
 import { useMemo } from "react";
 import { useApiClient } from "../useApiClient";
-import type { addSongToPlaylistDto, createPlaylistDto, deleteSongFromPlaylistDto, playlist, updatePlaylistDto } from "../dtos/playlist";
+import type {
+  addSongToPlaylistDto,
+  createPlaylistDto,
+  deleteSongFromPlaylistDto,
+  playlist,
+  updatePlaylistDto,
+} from "../dtos/playlist";
 import type { paginationParams } from "../dtos/paginationParams";
 
 export interface PlaylistsService {
   add(dto: createPlaylistDto): Promise<playlist>;
-  AddSongToPlaylist(dto: addSongToPlaylistDto):Promise<playlist>;
+  addSongToPlaylist(dto: addSongToPlaylistDto): Promise<playlist>;
   getAll(params: paginationParams): Promise<playlist[]>;
   getById(id: string): Promise<playlist>;
-  getPlaylistsOfUser(params:paginationParams):Promise<playlist[]>;
-  update(dto:updatePlaylistDto):Promise<playlist>;
-  deleteSongFromPlaylist(dto:deleteSongFromPlaylistDto):Promise<playlist>;
+  getPlaylistsOfUser(params: paginationParams): Promise<playlist[]>;
+  update(dto: updatePlaylistDto): Promise<playlist>;
+  deleteSongFromPlaylist(dto: deleteSongFromPlaylistDto): Promise<playlist>;
   delete(id: string): Promise<void>;
 }
 
-const defaultRoute="/playlists";
+const defaultRoute = "/playlists";
 
 export function usePlaylistsService(): PlaylistsService {
   const api = useApiClient();
 
   return useMemo(
     () => ({
-      add: (dto) => api.post<playlist>(`${defaultRoute}`, dto).then((r) => r.data),
-      
-      addSongToPlaylist:(dto)=> api.post<playlist>(`${defaultRoute}/${dto.playlistId}/songs/${dto.songId}`,...dto).then((r)=>r.data),
+      add: (dto) => api.post<playlist>(defaultRoute, dto).then((r) => r.data),
+
+      addSongToPlaylist: (dto) =>
+        api
+          .post<playlist>(
+            `${defaultRoute}/${dto.playlistId}/songs/${dto.songId}`,
+          )
+          .then((r) => r.data),
 
       getAll: (params) =>
-         api.get<playlist[]>(`${defaultRoute}`, { params }).then((r) => r.data),
+        api.get<playlist[]>(defaultRoute, { params }).then((r) => r.data),
 
       getById: (id) =>
         api.get<playlist>(`${defaultRoute}/${id}`).then((r) => r.data),
 
-      getPlaylistsOfUser:(params)=>api.get<playlist>(`${defaultRoute}`,{params}).then((r)=>r.data), 
+      getPlaylistsOfUser: (params) =>
+        api.get<playlist[]>(defaultRoute, { params }).then((r) => r.data),
 
-      update: (dto)=>api.put<playlist>(`${defaultRoute}/${dto.id},...dto`),
+      update: (dto) => {
+        const { id, ...body } = dto;
+        return api
+          .put<playlist>(`${defaultRoute}/${id}`, body)
+          .then((r) => r.data);
+      },
 
-      deleteSongFromPlaylist: (dto)=> api.delete<playlist>(`${defaultRoute}/${dto.playlistId}/songs/${dto.songId}`).then((r)=>r.data),
+      deleteSongFromPlaylist: (dto) =>
+        api
+          .delete<playlist>(
+            `${defaultRoute}/${dto.playlistId}/songs/${dto.songId}`,
+          )
+          .then((r) => r.data),
 
-      delete: (id) => api.delete<void>(`${defaultRoute}/${id}`).then((r) => r.data),
+      delete: (id) => api.delete<void>(`${defaultRoute}/${id}`).then(() => {}),
     }),
     [api],
   );
--- src/api/services/useSongsService.ts
+++ src/api/services/useSongsService.ts
@@ -4,29 +4,33 @@ import type { addSongDto, song, updateSongDto } from "../dtos/song";
 import type { paginationParams } from "../dtos/paginationParams";
 
 export interface SongsService {
-  add(dto:addSongDto):Promise<song>;
-  getAll(params:paginationParams): Promise<song[]>;
+  add(dto: addSongDto): Promise<song>;
+  getAll(params: paginationParams): Promise<song[]>;
   getById(id: string): Promise<song>;
-  update(dto:updateSongDto):Promise<song>;
-  delete(id:number):Promise<song>;
+  update(dto: updateSongDto): Promise<song>;
+  delete(id: number): Promise<void>;
 }
 
-const defaultRoute="/songs"
+const defaultRoute = "/songs";
 
 export function useSongsService(): SongsService {
   const api = useApiClient();
 
   return useMemo(
     () => ({
-      add: (dto)=>api.post<song>(defaultRoute,dto).then((r)=>r.data),
+      add: (dto) => api.post<song>(defaultRoute, dto).then((r) => r.data),
 
-      getAll: (params) => api.get<song[]>(defaultRoute,{params}).then((r) => r.data),
+      getAll: (params) =>
+        api.get<song[]>(defaultRoute, { params }).then((r) => r.data),
 
-      getById: (id) => api.get<song>(`${defaultRoute}/${id}`).then((r) => r.data),
-    
-      update: (dto)=> api.put<song>(`${defaultRoute}/${dto.id}`,...dto).then((r)=>r.data),
+      getById: (id) =>
+        api.get<song>(`${defaultRoute}/${id}`).then((r) => r.data),
 
-      delete:(id)=> api.delete<void>(`${defaultRoute}/${id}`).then((r) => r.data),
+      update: (dto) => {
+        const { id, ...body } = dto;
+        return api.put<song>(`${defaultRoute}/${id}`, body).then((r) => r.data);
+      },
+      delete: (id) => api.delete<void>(`${defaultRoute}/${id}`).then(() => {}),
     }),
     [api],
   );

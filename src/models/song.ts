fa410345659f3455b0e0fece2efe1d521b5a7180
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-23 22:29:37 +0300

    create song model

---
+++ src/models/song.ts
@@ -0,0 +1,10 @@
+export interface song {
+  id: string;
+  title: string;
+  album: string;
+  author: string;
+  authorId?: number;
+  duration: string;
+  addedAt?: string;
+  coverUrl: string;
+}

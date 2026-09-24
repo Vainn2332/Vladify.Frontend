db311271adf0e9e2cc64ed18fdcadd6b36a8b823
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-24 20:06:46 +0300

    update responsiveness

--- src/components/Sections/SongSection/SongSection.tsx
+++ src/components/Sections/SongSection/SongSection.tsx
@@ -18,13 +18,13 @@ export function SongSection({
   //TODO: СДЕЛАТЬ АДАПТИВНОСТЬ, СДЕЛАТЬ ПО ЦЕНТРУ НЕКОТОРЫЕ ПОЛЯ,ПОПРАВИТЬ PADDING
   return (
     <table className="sm:text-medium w-full table-fixed overflow-hidden rounded-xl bg-white/85 text-left text-xs">
-      <thead className="border-b border-black/10 text-xs text-black/50 uppercase">
+      <thead className="text-2xs border-b border-black/10 text-black/50 uppercase sm:text-xs">
         <tr>
-          <th className="w-8 p-4">#</th>
+          <th className="w-4 p-2 sm:p-4">#</th>
           <th className="p-4">Название</th>
           <th className="hidden p-4 sm:table-cell">Альбом</th>
-          {showAddedAt && <th className="">Дата добавления</th>}
-          <th className="w-20 p-4">
+          {showAddedAt && <th className="w-18 sm:w-auto">Дата добавления</th>}
+          <th className="w-10 p-4 sm:w-20">
             <Clock className="size-4" />
           </th>
           <th className="w-10 p-4" />
